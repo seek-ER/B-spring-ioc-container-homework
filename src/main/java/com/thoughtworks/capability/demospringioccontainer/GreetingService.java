4d86fff4c737ea8aa1d600d0081857e62aa7c7d0
Anson <1072476988@qq.com>
2020-11-09 00:32:47 +0800

    [Lingzhen Kong] feat: use proxyMode to handle.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("GreetingService created");
+    }
 
     String sayHi() {
         return "hello world";
